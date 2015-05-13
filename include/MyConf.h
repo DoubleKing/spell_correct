02258d7031d598902ca374e9d89148a977ef6044
DoubleKing <971774262@qq.com>
2015-05-13 01:13:54 -0700

    conf.h

---
+++ include/MyConf.h
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: MyConf.h
+	> Author: DoubleKing
+	> Mail:971774262@qq.com 
+	> Created Time: Wed 13 May 2015 12:57:59 AM PDT
+ ************************************************************************/
+#pragma once
+
+#include <map>
+#include <vector>
+#include <utility>
+
+namespace wd
+{
+class MyConf
+{
+
+};
+}
