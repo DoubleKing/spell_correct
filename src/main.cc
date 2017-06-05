f488d4f38d3396cff2d5d49861a8e21bb539c3e4
doubleking <971774262@qq.com>
2017-06-05 15:37:14 +0800

    deamon code

--- src/main.cc
+++ src/main.cc
@@ -7,10 +7,33 @@
 #include "TcpServer.h"
 #include "MyConf.h"
 #include "MyDic.h"
-#include<iostream>
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+void daemonize(void) {
+    int fd;
 
-int main()
+    if (fork() != 0) exit(0); /* parent exits */
+    setsid(); /* create a new session */
+
+    /* Every output goes to /dev/null. If Redis is daemonized but
+     * the 'logfile' is set to 'stdout' in the configuration file
+     * it will not log at all. */
+    if ((fd = open("/dev/null", O_RDWR, 0)) != -1) {
+        dup2(fd, STDIN_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        dup2(fd, STDERR_FILENO);
+        if (fd > STDERR_FILENO) close(fd);
+    }
+}
+
+int main(int argc, char *argv[])
 {
+	if(argc > 1 && strcmp(argv[1],"daemon") == 0)
+		daemonize();
 	wd::MyConf conf("./conf/conf.txt");
 	conf.conf_show();
 	//wd::MyDic dic(conf);
