b27acd7303af92e0374de077cd22d2b3c03e45e9
doubleking <971774262@qq.com>
2018-08-03 08:50:41 +0800

    remove the comments

--- src/main.cc
+++ src/main.cc
@@ -25,9 +25,6 @@ void daemonize(void) {
     if (fork() != 0) exit(0); /* parent exits */
     setsid(); /* create a new session */
 
-    /* Every output goes to /dev/null. If Redis is daemonized but
-     * the 'logfile' is set to 'stdout' in the configuration file
-     * it will not log at all. */
     if ((fd = open("/dev/null", O_RDWR, 0)) != -1) {
         dup2(fd, STDIN_FILENO);
         dup2(fd, STDOUT_FILENO);
